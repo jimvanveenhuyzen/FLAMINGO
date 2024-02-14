9a50b146c70511886292dec080f76097f2d3cdfd
jimvanveenhuyzen <125078335+jimvanveenhuyzen@users.noreply.github.com>
2024-02-14 21:52:55 +0100

    Add files via upload

--- power_spectra/1DgenPower.py
+++ power_spectra/1DgenPower.py
@@ -3,32 +3,46 @@ import matplotlib.pyplot as plt
 from nbodykit.source.catalog import ArrayCatalog
 from nbodykit.lab import *
 from nbodykit import setup_logging, style
+import dask.array as da
 import h5py
 import sys
 
 def plot_positions(positions,rsd_positions):
 
     #Create a filter so that we dont have to plot all halos (very expensive)
-    condition_z = (positions[:, 2] > 100) & (positions[:, 2] < 110)
-    condition_y = (positions[:, 1] > 100) & (positions[:, 1] < 110)
-    condition_x = positions[:, 0] < 150
+    condition_z = (positions[:, 2] > 510) & (positions[:, 2] < 610)
+    condition_y = (positions[:, 1] > 300) & (positions[:, 1] < 470)
+    condition_x = positions[:, 0] < 20
 
     pos = positions[condition_x & condition_y & condition_z]
     rsdpos = rsd_positions[condition_x & condition_y & condition_z]
 
-    plt.scatter(pos[:,1],pos[:,2],s=10,color='black',label='Positions',zorder=20)
-    plt.scatter(rsdpos[:,1],rsdpos[:,2],s=10,color='red',label='RSDPositions',zorder=10)
-    #for i in range(len(pos[:,1])):
-    #    print(i)
-    #    plt.plot([rsdpos[i,1],rsdpos[i,1]],[pos[i,2],rsdpos[i,2]],color='skyblue',linestyle='dashed',zorder=1)
+    plt.scatter(pos[:,1],pos[:,2],s=3,color='black',label='Positions',zorder=5)
+    plt.scatter(rsdpos[:,1],rsdpos[:,2],s=3,color='firebrick',label='RSDPositions',zorder=10)
+    count1 = 0
+    count2 = 0
+    for i in range(len(pos[:,1])):
+        if pos[i,2] < rsdpos[i,2]: 
+            if count1 == 0:
+                plt.plot([rsdpos[i,1],rsdpos[i,1]],[pos[i,2],rsdpos[i,2]],color='darkblue',linewidth=1.,zorder=1,label='Shift upward')
+                count1 += 1 
+            else:
+                plt.plot([rsdpos[i,1],rsdpos[i,1]],[pos[i,2],rsdpos[i,2]],color='darkblue',linewidth=1.,zorder=1)
+        else:
+            if count2 == 0:
+                plt.plot([rsdpos[i,1],rsdpos[i,1]],[pos[i,2],rsdpos[i,2]],color='darkgreen',linewidth=1.,zorder=1,label='Shift downward')
+                count2 += 1 
+            else: 
+                plt.plot([rsdpos[i,1],rsdpos[i,1]],[pos[i,2],rsdpos[i,2]],color='darkgreen',linewidth=1.,zorder=1)
     plt.grid()
-    plt.xlim([100,110])
-    plt.ylim([100,110])
+    #plt.xlim([100,110])
+    plt.ylim([500,640])
     plt.xlabel('y [Mpc]')
     plt.ylabel('z [Mpc]')
-    plt.legend(loc='upper right')
-    plt.title('(y,z) coordinates of the galaxies')
-    #plt.savefig('rsd_effectPos.png')
+    plt.tick_params(axis='both', which='both', direction='in', bottom=True, top=True, left=True, right=True)
+    plt.legend(loc='upper right',ncol=2)
+    plt.title('Clustering of galaxies due to the RSD effect along z-axis')
+    plt.savefig('RSDeffect_cluster.png')
     plt.close()
 
     #Play around with these masks to find a proper cluster to plot: 
@@ -74,6 +88,25 @@ print(H_a0)
 def add_RSD(catalogue,z,line_of_sight):
     a = 1/(1+z)
     rsd_pos = catalogue['Position'] + (catalogue['Velocity'] * line_of_sight)/(a*H_a0)
+
+    #Before we return the RSD Positions, we add the periodicity: positions cannot exceed 1000 or drop below 0
+    #First, determine the axis of which the RSD effect is added 
+    los_idx = int(np.nonzero(line_of_sight)[0])
+
+    #The catalogue entries are Dask arrays, so we need to convert to numpy to perform vector operations
+    rsd_pos_numpy = rsd_pos.compute()
+
+    #Second, we use two conditions to 'reverse' the positional distances that exceed 1000 or drop below 0
+    mask_low = np.where(rsd_pos_numpy[:,los_idx] < 0)[0]
+    mask_high = np.where(rsd_pos_numpy[:,los_idx] > 1000)[0]
+
+    #Apply the masks:
+    rsd_pos_numpy[mask_low,los_idx] = 1000. - (1000.+rsd_pos_numpy[mask_low,los_idx]) 
+    rsd_pos_numpy[mask_high,los_idx] = 2*1000. - rsd_pos_numpy[mask_high,los_idx] #This works assuming the RSD Positions are not >2000, which they are never
+
+    #Finally, convert back to a dask dataframe column 
+    rsd_pos = da.from_array(rsd_pos_numpy,chunks=rsd_pos.chunks)
+
     return rsd_pos
 
 def genPower_1D(position,mass,velocity,Nmesh,dk,kmin,show_data):
@@ -101,10 +134,10 @@ def genPower_1D(position,mass,velocity,Nmesh,dk,kmin,show_data):
     RSDPosition = add_RSD(cat,z,line_of_sight)
     cat['RSDPosition'] = RSDPosition
 
-    print(cat['Position'].compute())
-    print(cat['RSDPosition'].compute())
+    #print(cat['Position'].compute())
+    #print(cat['RSDPosition'].compute())
 
-    plot_positions(cat['Position'].compute(),cat['RSDPosition'].compute())
+    #plot_positions(cat['Position'].compute(),cat['RSDPosition'].compute())
 
     #convert to a MeshSource, using TSC interpolation on Nmesh^3 
     mesh = cat.to_mesh(window='tsc', Nmesh=Nmesh, compensated=True, position='RSDPosition', BoxSize=[1000,1000,1000])
@@ -129,15 +162,14 @@ galaxy_filterVel = np.load('/net/draco/data2/vanveenhuyzen/rsd_project/particle_
 galaxy_filterMass = np.load('/net/draco/data2/vanveenhuyzen/rsd_project/particle_data/galaxy_filterMass.npy')
 galaxy_filterPos = np.load('/net/draco/data2/vanveenhuyzen/rsd_project/particle_data/galaxy_filterPos.npy')
 
-k_1D, Pk_1D, shotnoise_1D = genPower_1D(galaxy_filterPos,galaxy_filterMass,galaxy_filterVel,512,0.01,0.001,False)
-"""
+k_1D, Pk_1D, shotnoise_1D = genPower_1D(galaxy_filterPos,galaxy_filterMass,galaxy_filterVel,512,0.001,0.001,False)
+
 Pk_noSN_1D = Pk_1D-shotnoise_1D
-print(np.shape(Pk_noSN_1D))
-print(k_1D)
-print(Pk_noSN_1D)
+#print(np.shape(Pk_noSN_1D))
+#print(k_1D)
+#print(Pk_noSN_1D)
 
 print('Saving the 1D data...')
 np.save('1Dpower_k.npy',k_1D)
 np.save('1Dpower_pk.npy',Pk_noSN_1D)
 print('Done saving!')
-"""
